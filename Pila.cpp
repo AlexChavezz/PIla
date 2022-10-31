df7f6570974a1e6c1e8c1c279b962580664b10c2
AlexChavezz <alexischavez1234.ac@gmail.com>
2022-10-30 18:11:14 -0600

    newpush

--- Pila.cpp
+++ Pila.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
-class Pila {
+class Pila
+{
 private:
-	class Node {
+	class Node
+	{
 	public:
 		char letter;
-		Node* next;
+		Node *next;
+		Node(int d)
+		{
+			letter = d;
+			next = NULL;
+		}
 	};
-	Node* pila;
+	Node *pila;
+
 public:
 	Pila();
 	void deleteNode();
@@ -16,70 +24,78 @@ public:
 	void print();
 };
 
-Pila::Pila() {
+Pila::Pila()
+{
 	pila = NULL;
 }
 
-void Pila::addNewNode(char l){
-	Node* newNode = new Node();
-	newNode->letter = l;
+void Pila::addNewNode(char l)
+{
+	Node *newNode = new Node(l);
 	newNode->next = pila;
 	pila = newNode;
-	cout <<l<<" has be added to pila successfuly" << endl;
+	cout << l << " has be added to pila successfuly" << endl;
 }
 
-void Pila::print() {
-	Node* aux;
+void Pila::print()
+{
+	Node *aux;
 	for (aux = pila; aux != NULL; aux = aux->next)
 	{
 		cout << aux->letter << endl;
 	}
 }
 
-void Pila::deleteNode() {
+void Pila::deleteNode()
+{
 	int l;
-	Node* aux = pila;
+	Node *aux = pila;
 	l = aux->letter;
 	pila = aux->next;
 	cout << aux->letter << " has be eliminated" << endl;
 	delete aux;
 }
 
-int main() {
+int main()
+{
 	char letter;
 	int iterations;
 	cout << "How many elements you need add" << endl;
 	cin >> iterations;
 
-	Pila* object = new Pila();
-	for (int i = 0; i < iterations; i++) {
+	Pila *object = new Pila();
+	for (int i = 0; i < iterations; i++)
+	{
 		cout << "Enter letter to add: " << endl;
 		cin >> letter;
 		object->addNewNode(letter);
 	}
 	int opc;
+	bool flag = true;
 
-	do {
-		cout << "What do now?" << endl;
+	do
+	{
+		cout << "What do you want to do now?" << endl;
 		cout << "(1) remove one\n(2) print\n(3) exit" << endl;
-		cout << ": ";
 		cin >> opc;
-		
-		switch (opc)
+
+		if (opc == 1)
 		{
-		case 1:
 			object->deleteNode();
-			break;
-		case 2:
+		}
+		else if (opc == 2)
+		{
 			object->print();
-			break;
-		case 3:
-			break;
-		default:
-			break;
+		}
+		else if (opc == 3)
+		{
+			flag = false;
+		}
+		else
+		{
+			cout << "Invalid option" << endl;
 		}
 	} while (opc != 3);
-
 
 	return 0;
 }
